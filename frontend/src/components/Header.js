434919eab8747a424e5dd376e6d751a47636a681
KAPIL DAHIYA <kapildahiya309@gmail.com>
2024-10-20 19:21:35 +0530

    Created footer and creating Home section

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
+import compareImage from "../images/images/compare.svg"
+import wishlistImage from "../images/images/wishlist.svg"
+import userImage from "../images/images/user.svg"
+import cartImage from "../images/images/cart.svg"
 const Header = () => {
   return (
     <>
@@ -41,7 +45,7 @@ const Header = () => {
                   aria-describedby="basic-addon2"
                 />
                 <span class="input-group-text p-3" id="basic-addon2">
-                  <BsSearch className="fs-6"/>
+                  <BsSearch className="fs-6" />
                 </span>
                 /
               </div>
@@ -49,24 +53,88 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link>
-                  <img src="images/compare.svg" alt="" /></Link>
-                  <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={compareImage} alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
                 </div>
                 <div>
-                  <Link>
-                  <img src="" alt="" /></Link>
-                  <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={wishlistImage} alt="wishlist" />
+                    <p className="mb-0">
+                      {" "}
+                      Favourite <br /> Wishlist
+                    </p>
+                  </Link>
                 </div>
                 <div>
-                  <Link>
-                  <img src="" alt="" /></Link>
-                  <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={userImage} alt="login" />
+                    <p className="mb-0">
+                      Log In <br /> My Account
+                    </p>
+                  </Link>
                 </div>
                 <div>
-                  <Link>
-                  <img src="" alt="" /></Link>
-                  <p></p>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src={cartImage} alt="cart" />
+                    <div className="d-flex flex-column">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="col-12">
+            <div className="menu-bottom d-flex align-items-center gap-30">
+              <div>
+                <div class="dropdown">
+                  <button
+                    class="btn btn-secondary dropdown-toggle bg-transparent border-0"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img src="images/images/menu.svg" alt="" />
+                    <span className="me-5 d-inline-block">Shop Categories</span>
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton1"
+                  >
+                    <li>
+                      <Link className="dropdown-item text-white" to="">
+                        Action
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item text-white" to="">
+                        Another action
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item text-white" to="">
+                        Something else here
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="menu-links">
+                <div className="d-flex align-items-center gap-15">
+                  <NavLink to="/">Home</NavLink>
+                  <NavLink to="/">Out Store</NavLink>
+                  <NavLink to="/">Blogs</NavLink>
+                  <NavLink to="/contact">Contact</NavLink>
                 </div>
               </div>
             </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import mainbanner from "../images/images/main-banner-1.jpg";
+import catbanner from "../images/images/catbanner-01.jpg"
+
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <section className="home-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative p-3">
+                <img
+                  src={mainbanner}
+                  alt="main banner"
+                  className="img-fluid rounded-3"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>Supercharged for pros.</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.00 or $41.62/mo</p>
+                  <Link className="button">Buy Now</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+            <div className="small-banner position-relative p-3">
+                <img
+                  src={catbanner}
+                  alt="main banner"
+                  className="img-fluid rounded-3"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>Supercharged for pros.</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.00 or $41.62/mo</p>
+                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
